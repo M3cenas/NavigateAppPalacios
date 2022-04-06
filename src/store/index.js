e6ee3a44daec47dac3710a6ee29d705755205399
LAIN <jrafael.pfz@gmail.com>
2022-04-06 17:22:28 -0400

    [feat] - add redux thunk & implement middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,11 @@
 import {createStore, combineReducers} from 'redux';
 import genreReducer from './reducers/genre.reducer';
 import albumReducer from './reducers/album.reducer';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   genres: genreReducer,
   albums: albumReducer,
 });
 
-export default createStore(rootReducer);
+export default createStore(rootReducer, applyMiddleware(thunk));
---
+++ src/utils/database.js
@@ -0,0 +1,1 @@
+export const URL_API = 'https://albumshop-abacb-default-rtdb.firebaseio.com/';
